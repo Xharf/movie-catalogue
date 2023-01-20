eeca6a0178e6d797ff30d1567bc081a0cc2a9277
xharf <123190123@student.upnyk.ac.id>
2023-01-20 10:21:17 +0700

    Add dotenv-webpack and reconfigure webpack and karma

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'dd1b95bc1f22bfa17b4e2f169728ba3b',
+  KEY: process.env.API_KEY_TMDB,
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
